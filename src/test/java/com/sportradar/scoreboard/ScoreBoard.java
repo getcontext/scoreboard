a308b03767f6531f89935afdb4944ffb68cdb069
wizard <andrzej.salamon@gmail.com>
2026-02-18 21:42:21 +0100

    generate empty methods and constructors

--- src/test/java/com/sportradar/scoreboard/ScoreBoard.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoard.java
@@ -1,6 +1,32 @@
 package com.sportradar.scoreboard;
 
+import java.util.List;
+
 public class ScoreBoard {
-    public class GameInfo {
+    public void startGame(String mexico, String canada) {
+    }
+
+    public List<GameInfo> getSummary() {
+    }
+
+    public void finishGame(String germany, String france) {
+    }
+
+    public void updateScore(String uruguay, String italy, int i, int i1) {
+    }
+
+    public static class GameInfo {
+        public String homeTeam;
+        public int homeScore;
+        public String awayTeam;
+        public int awayScore;
+
+        public GameInfo(String homeTeam, int homeScore,
+                        String awayTeam, int awayScore) {
+            this.homeTeam = homeTeam;
+            this.homeScore = homeScore;
+            this.awayTeam = awayTeam;
+            this.awayScore = awayScore;
+        }
     }
 }
