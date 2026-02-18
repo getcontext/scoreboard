db9630ef06d5abd8d1ac3dfb798d24dfd14e35de
wizard <andrzej.salamon@gmail.com>
2026-02-18 21:47:00 +0100

    override default methods from Object class into GameInfo dto class. add equals and hashcode to prevent collisions. add toString for prettyprint

--- src/test/java/com/sportradar/scoreboard/ScoreBoard.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoard.java
@@ -1,6 +1,7 @@
 package com.sportradar.scoreboard;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ScoreBoard {
     public void startGame(String mexico, String canada) {
@@ -15,7 +16,8 @@ public class ScoreBoard {
     public void updateScore(String uruguay, String italy, int i, int i1) {
     }
 
-    public static class GameInfo {
+    /** Simple DTO returned by {@link #getSummary()}. */
+    public static final class GameInfo {
         public String homeTeam;
         public int homeScore;
         public String awayTeam;
@@ -27,6 +29,28 @@ public class ScoreBoard {
             this.homeScore = homeScore;
             this.awayTeam = awayTeam;
             this.awayScore = awayScore;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("%s %d - %s %d",
+                    homeTeam, homeScore, awayTeam, awayScore);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof GameInfo)) return false;
+            GameInfo other = (GameInfo) o;
+            return homeScore == other.homeScore &&
+                    awayScore == other.awayScore &&
+                    homeTeam.equals(other.homeTeam) &&
+                    awayTeam.equals(other.awayTeam);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(homeTeam, homeScore, awayTeam, awayScore);
         }
     }
 }
