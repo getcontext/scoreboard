b7f5b4538b0e953f314763a874f09add29bf5254
wizard <andrzej.salamon@gmail.com>
2026-02-18 20:14:37 +0100

    tests setup

--- src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -1,4 +1,13 @@
 package com.sportradar.scoreboard;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class ScoreBoardTest {
+
+    private ScoreBoard board;
+
+    @BeforeEach
+    void setUp() {
+        board = new ScoreBoard();
+    }
 }
