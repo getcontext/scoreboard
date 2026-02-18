d4fbbac8dca7a714fe749399f54c3cf6d5a956a1
wizard <andrzej.salamon@gmail.com>
2026-02-18 20:15:57 +0100

    add startGameAddsGameWithZeroZeroScore tests

--- src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
+++ src/test/java/com/sportradar/scoreboard/ScoreBoardTest.java
@@ -1,6 +1,11 @@
 package com.sportradar.scoreboard;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ScoreBoardTest {
 
@@ -9,5 +14,17 @@ public class ScoreBoardTest {
     @BeforeEach
     void setUp() {
         board = new ScoreBoard();
+    }
+
+    @Test
+    void startGameAddsGameWithZeroZeroScore() {
+        board.startGame("Mexico", "Canada");
+        List<ScoreBoard.GameInfo> summary = board.getSummary();
+        assertEquals(1, summary.size());
+        ScoreBoard.GameInfo g = summary.get(0);
+        assertEquals("Mexico", g.homeTeam);
+        assertEquals(0, g.homeScore);
+        assertEquals("Canada", g.awayTeam);
+        assertEquals(0, g.awayScore);
     }
 }
